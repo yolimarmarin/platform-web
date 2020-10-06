017d069fad8ecedd939c8bb10289455adeeecf4a
Yolimar Marin <yolimar.ma83@gmail.com>
2020-10-06 11:58:50 -0500

    Updates

---
+++ precache-manifest.447ac3f8a47dd639347670f1c30454c7.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be261cc64bee1e23061bfc6938a8d8fa",
+    "url": "/platform-web/index.html"
+  },
+  {
+    "revision": "fcaeb5ab8d216a6eb7a0",
+    "url": "/platform-web/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "9de11283ae68b5f74ddc",
+    "url": "/platform-web/static/css/main.497d9391.chunk.css"
+  },
+  {
+    "revision": "fcaeb5ab8d216a6eb7a0",
+    "url": "/platform-web/static/js/2.71df72a1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/platform-web/static/js/2.71df72a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9de11283ae68b5f74ddc",
+    "url": "/platform-web/static/js/main.42ea9697.chunk.js"
+  },
+  {
+    "revision": "55ac6e0d1f68cbd9da54",
+    "url": "/platform-web/static/js/runtime-main.2abcf2c6.js"
+  },
+  {
+    "revision": "889a4b5ce3a780faedb74390fc49a07a",
+    "url": "/platform-web/static/media/Gilroy-Bold.889a4b5c.ttf"
+  },
+  {
+    "revision": "0f6e082f42f5247d82c3ade9c2a8f928",
+    "url": "/platform-web/static/media/Gilroy-ExtraBold.0f6e082f.otf"
+  },
+  {
+    "revision": "c62aded729bf7146d491275e5019d7fc",
+    "url": "/platform-web/static/media/Gilroy-Light.c62aded7.otf"
+  },
+  {
+    "revision": "d8ee4539e9ba9211a8b532e325075577",
+    "url": "/platform-web/static/media/Gilroy-Medium.d8ee4539.ttf"
+  },
+  {
+    "revision": "22d25e11ebe6b43e5a9bc87d0a0ccb61",
+    "url": "/platform-web/static/media/Gilroy-Regular.22d25e11.ttf"
+  },
+  {
+    "revision": "77171d8f5b5283f9d47a3434704bf944",
+    "url": "/platform-web/static/media/GothamMedium.77171d8f.ttf"
+  },
+  {
+    "revision": "5e8c235582bb04368d80f058b591221f",
+    "url": "/platform-web/static/media/arrow-left.5e8c2355.svg"
+  },
+  {
+    "revision": "85bbf35b2a06a91364e2528206c0803a",
+    "url": "/platform-web/static/media/arrow-right.85bbf35b.svg"
+  },
+  {
+    "revision": "c5ffc2ac80b57b90a047fac85a1dff60",
+    "url": "/platform-web/static/media/banner-blue.c5ffc2ac.png"
+  },
+  {
+    "revision": "246bfa0b84588a289a10478bea8197b4",
+    "url": "/platform-web/static/media/banner-figure.246bfa0b.png"
+  },
+  {
+    "revision": "25b812f52f9d98699734d134af992bc3",
+    "url": "/platform-web/static/media/logo.25b812f5.svg"
+  },
+  {
+    "revision": "1608d9d8247c4e25a2efcb240f5f19cb",
+    "url": "/platform-web/static/media/menu-grey.1608d9d8.svg"
+  },
+  {
+    "revision": "590f7a385f1727b8519c6a6f305d891c",
+    "url": "/platform-web/static/media/menu.590f7a38.svg"
+  },
+  {
+    "revision": "11d8df66a249bf411bc3f3a680768434",
+    "url": "/platform-web/static/media/profile.11d8df66.png"
+  },
+  {
+    "revision": "31ff7c1a62a300dbbf9656b4ba14a0d5",
+    "url": "/platform-web/static/media/regular.31ff7c1a.ttf"
+  },
+  {
+    "revision": "425959f5b03b566918060f30d05a5c71",
+    "url": "/platform-web/static/media/search.425959f5.svg"
+  },
+  {
+    "revision": "cb4f3c820f0f5de8489bc6fb0ea4e0fd",
+    "url": "/platform-web/static/media/slider-bg.cb4f3c82.png"
+  }
+]);
